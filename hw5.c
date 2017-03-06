260eca9b52f966f5e19c1286c2b0db785058a6cd
qpmullig <qpmullig@syr.edu>
2017-03-05 20:42:45 -0500

    Update hw5.c

--- hw5.c
+++ hw5.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]){
 	printf("Hello World\n");
+	int opt = getopt(argc, argv, "hdmt");
+	
+	while(opt != -1){
+		
+		opt = getopt(argc, argv, "hdmt");
+	}
 }
