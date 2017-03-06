60f20d442b1627109a237f6b54a9a18edfde2198
msperez217 <msperez@syr.edu>
2017-03-05 22:13:25 -0500

    added flags for getopt

--- hw5.c
+++ hw5.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
 
 int main(int argc, char* argv[]){
-	printf("Hello World\n");
-	int opt = getopt(argc, argv, "hdmt");
-	
+	bool enable_h = false;
+	bool enable_d = false;
+	bool enable_m = false;
+	bool enable_t = false;
+	char* d_arg = NULL;
+	int opt = getopt(argc, argv, "hd:mt");
 	while(opt != -1){
-		
-		opt = getopt(argc, argv, "hdmt");
+		switch(opt){
+			case 'h':
+			enable_h = true;
+			break;
+			case 'd':
+			enable_d = true;
+			d_arg = strdup(optarg);
+			break;
+			case 'm':
+			enable_m = true;
+			break;
+			case 't':
+			enable_t = true;
+			break;
+		}
+		opt = getopt(argc, argv, "hd:mt");
 	}
 }
