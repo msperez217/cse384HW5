71042c726c5350a5a53b644c640b130903fe3abe
qpmullig <qpmullig@syr.edu>
2017-03-08 20:04:51 -0500

    Implimented -d option

--- hw5.c
+++ hw5.c
@@ -6,7 +6,7 @@
 #include <sys/inotify.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <errno.h>
+#include <libgen.h>
 
 int main(int argc, char* argv[]){
 	bool enable_h = false, enable_m = false, enable_t = false, enable_d_arg = false, enable_error = false;
@@ -55,6 +55,7 @@ int main(int argc, char* argv[]){
 	if(enable_d_arg == true){
 		printf("ERROR: Option -d requires an argument.\n");
 		return EXIT_FAILURE;
+
 	}
 	if(argv[optind] == NULL){
 		printf("No argument given.\n");
@@ -74,7 +75,21 @@ int main(int argc, char* argv[]){
 	if(wd == -1){
 		perror("inotify_add_watch");
 		return EXIT_FAILURE;
+
 	}
+
+	char* file_name = basename(argv[optind]);
+	char file_loc[100];
+	if(d_arg == NULL){
+		char file_origin[100];
+		snprintf(file_origin, 100,"%s",argv[optind]);
+		snprintf(file_loc, 100,"%s/%s",dirname(file_origin),file_name);
+	}
+	else{
+		snprintf(file_loc, 100,"%s/%s",d_arg,file_name);
+	}
+	printf("File was sent to %s\n", file_loc);
+
 
 	int y = open(file, O_RDWR);
 	const size_t data_size = 100;
@@ -82,7 +97,7 @@ int main(int argc, char* argv[]){
 	const size_t size = 5;
 	char d[size];
 	char* p;
-	sprintf(buffer, "%s_rev%d",file,rev);
+	sprintf(buffer, "%s_rev%d",file_loc,rev);
 	int x, num_bytes_read = 1;
 	int backup = open(buffer,  O_RDWR | O_CREAT | O_TRUNC, 
 		S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
@@ -102,7 +117,7 @@ int main(int argc, char* argv[]){
 			struct inotify_event* event = (struct inotify_event*)p;
 			if((event->mask & IN_MODIFY) != 0){
 				rev++;
-				sprintf(buffer, "%s_rev%d",file, rev);
+				sprintf(buffer, "%s_rev%d",file_loc, rev);
 				backup = open(buffer, O_RDWR | O_CREAT | O_TRUNC, 
 					S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 				num_bytes_read = 1;
